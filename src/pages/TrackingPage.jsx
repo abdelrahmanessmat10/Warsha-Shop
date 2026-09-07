24f6c46edb2f8e078b843ef62e0ab6f513885b0e
abdelrahmanessmat10 <23-101348@students.eui.edu.eg>
2026-09-07 14:00:11 +0300

    Use Link in all the pages

--- src/pages/TrackingPage.jsx
+++ src/pages/TrackingPage.jsx
@@ -1,4 +1,4 @@
-
+import { Link } from 'react-router'
 import './TrackingPage.css'
 import { Header } from '../components/Header';
 export function TrackingPage() {
@@ -9,9 +9,9 @@ export function TrackingPage() {
 
       <div className="tracking-page">
         <div className="order-tracking">
-          <a className="back-to-orders-link link-primary" href="/orders">
+          <Link className="back-to-orders-link link-primary" to="/orders">
             View all orders
-          </a>
+          </Link>
 
           <div className="delivery-date">
             Arriving on Monday, June 13
