8ad562a7b266f231bb39422bce2d33dcd335f00d
abdelrahmanessmat10 <23-101348@students.eui.edu.eg>
2026-09-10 01:40:15 +0300

    Create ProductGrid component and split checkotPage into smaller components

---
+++ Frontend/src/pages/checkout/OrderSummary.jsx
@@ -0,0 +1,35 @@
+import { DeliveryOption } from './DliveryOptions';
+import { CartItemDetails } from './CartItemDetails';
+import { DeliveryDate } from './DeliveryDate';
+
+
+export function OrderSummary({ deliveryOptions, cart }) {
+  return (
+    <div className="order-summary">
+      {deliveryOptions.length > 0 && cart.map((cartItem) => {
+        const selectedDeliveryOption = deliveryOptions.find((deliveryOption) => {
+          return deliveryOption.id === cartItem.deliveryOptionId;
+        });
+        return (
+          <>
+            <div key={cartItem.productId} className="cart-item-container">
+              <DeliveryDate selectedDeliveryOption={selectedDeliveryOption} />
+
+              <div className="cart-item-details-grid">
+                <img className="product-image"
+                  src={cartItem.product.image} />
+
+                <CartItemDetails cartItem={cartItem} />
+
+                <DeliveryOption deliveryOptions={deliveryOptions} cartItem={cartItem} />
+              </div>
+            </div>
+          </>
+        );
+
+      })}
+
+    </div>
+
+  );
+}
