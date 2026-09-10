ad2be8e054fae3847f014555a6253c77d0c5e191
abdelrahmanessmat10 <23-101348@students.eui.edu.eg>
2026-09-10 12:11:05 +0300

    Separate OrdersPage into smaller components

---
+++ Frontend/src/pages/orders/OrdersPage.jsx
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import { useEffect, useState, Fragment } from 'react'
+import { Header } from '../../components/Header';
+import './OrdersPage.css'
+import { Form, Link } from 'react-router'
+import { OrderHeader } from './OrderHeader';
+import { OrderDetailsGrid } from './OrderDetailsGrid';
+
+export function OrdersPage({ cart }) {
+  const [orders, setOrders] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/orders?expand=products').then((response) => {
+      setOrders(response.data)
+    });
+  }, []);
+  return (
+    <>
+      <title>Orders</title>
+      <Header cart={cart} />
+
+      <div className="orders-page">
+        <div className="page-title">Your Orders</div>
+
+        <div className="orders-grid">
+          {orders.length > 0 && orders.map((order) => {
+            return (
+              <div key={order.id} className="order-container">
+
+                <OrderHeader order={order} />
+
+                <OrderDetailsGrid order={order} />
+
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
